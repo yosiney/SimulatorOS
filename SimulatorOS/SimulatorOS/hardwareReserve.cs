2d3d90fcf26fdc6c5e4c993d9031ce2b58947365
joser40 <96205085+Lejoser40@users.noreply.github.com>
2024-04-26 18:01:53 -0600

    Modo Realista Listo

--- SimulatorOS/SimulatorOS/hardwareReserve.cs
+++ SimulatorOS/SimulatorOS/hardwareReserve.cs
@@ -9,22 +9,31 @@ namespace SimulatorOS
     internal class hardwareReserve
     {
 
-        private static List<long> reserva1;
-        private static List<long> reserva2;
+        private static List<Double> reserva1;
+        private static List<Double> reserva2;
+        private static List<Double> reserva3;
 
         public static void activar()
         {
-            reserva1 = new List<long>();
-            reserva2 = new List<long>();
+            reserva1 = new List<Double>();
+            reserva2 = new List<Double>();
+            reserva3 = new List<Double>();
 
             for (int i = 0; i < 100000000; i++)
             {
                 reserva1.Add(i);
             }
 
-            for (int i = 0; i < 100000000; i++)
+            for (int i = 0; i < 10000000; i++)
             {
-                reserva2.Add(i);
+                Double a = (i * 2) * 2;
+                reserva2.Add(a);
+            }
+
+            for (int i = 0; i < 10000000; i++)
+            {
+                Double a = (i * 2) * 2;
+                reserva3.Add(a);
             }
         }
 
@@ -32,8 +41,10 @@ namespace SimulatorOS
         {
             reserva1.Clear();
             reserva2.Clear();
+            reserva3.Clear();
             reserva1 = null;
             reserva2 = null;
+            reserva3 = null;
             GC.Collect();
         }
     }
